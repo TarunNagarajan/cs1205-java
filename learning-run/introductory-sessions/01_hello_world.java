e02111c1ec779ae024d5ccee531cf03b9bbc6b40
Tarun Nagarajan <tarun.greenville@gmail.com>
2025-04-16 21:48:28 +0530

    hello world - first java program

---
+++ learning-run/introductory-sessions/01_hello_world.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // This is my first Java Program
+
+        /* This is my first
+        multi-line comment */
+
+        System.out.println("I like pizza!");
+        System.out.println("It's really good!");
+        System.out.println("Buy me pizza!");
+    }
+}
